d356850417005474744df92a61c22ea3f666e5d3
Simone Vetere <simoneveterex@gmail.com>
2024-11-08 17:08:52 +0100

    fix vari

--- script.js
+++ script.js
@@ -5,6 +5,13 @@ document.body.onselectstart = function() {
 const canvas = document.getElementById('gameCanvas');
 var lives = 3;
 
+const fireballs = [];
+let powerupActive = false;
+let powerupStartTime;
+let fireballRadius = 5;
+let fireballSpeed = 20;
+let canShoot = true; // Flag to control shooting cooldown
+
 window.scrollTo(0, document.body.scrollHeight); 
 
 window.addEventListener('scroll', 
@@ -21,6 +28,7 @@ canvas.width = window.innerWidth;
 
 const ctx = canvas.getContext('2d');
 let speedHard = 1;
+let playerSpeed = 2;
 let points = 0;
 
 var playerImage = [];
@@ -41,6 +49,12 @@ for (var i = 1; i < 64; i++) {
   obstacleImage[i].src = "img/chicken/"+i+".png";
 }
 
+var fireballImage = [];
+for (var i = 1; i < 4; i++) {
+  fireballImage[i] = new Image();
+  fireballImage[i].src = "img/fireball/"+i+".png";
+}
+
 const meteorImage = new Image();
 meteorImage.src = 'img/meteor.png';
 let meteorAngle = 0;
@@ -52,7 +66,7 @@ const player = {
     y: 300,
     width: 50,
     height: 50,
-    speed: 2,
+    speed: playerSpeed,
     dx: 0,
     dy: 0,
     gravity: 0.5,
@@ -113,7 +127,15 @@ function newPos() {
 
 function updateObstacles() {
     for (let i = obstacles.length - 1; i >= 0; i--) {
+        
         obstacles[i].x -= obstacles[i].speed;
+
+        obstacles[i].y -= obstacles[i].direction;
+
+        // Cambia direzione ogni tot frame
+        if (obstacles[i].x % 50 === 0) {
+            obstacles[i].direction *= -1;
+        }
         if (obstacles[i].x + obstacles[i].width < 0) {
             obstacles.splice(i, 1);
         }
@@ -129,6 +151,7 @@ function createObstacle() {
         y: randomFactor,
         height : 64,
         width : 64,
+        direction : Math.random() > 0.5 ? 1 : -1,
         speed: speedHard
     });
 }
@@ -136,7 +159,10 @@ function createObstacle() {
 function updateMeteors() {
 
     for (let i = meteore.length - 1; i >= 0; i--) {
-        meteore[i].y += meteore[i].speed;
+
+        meteore[i].x += Math.cos(meteore[i].angle) * meteore[i].speed;
+        meteore[i].y += Math.sin(meteore[i].angle) * meteore[i].speed;
+
         if (meteore[i].y - meteore[i].radius > canvas.height) {
             meteore.splice(i, 1);
         }
@@ -151,39 +177,127 @@ function createMeteor() {
         x: Math.random() * canvas.width,
         y: -radius,
         radius: radius,
+        angle: Math.random() * Math.PI,
         speed: speedHard
     });
 }
 
+let canShoottimer = 100;
+function createFireball() {
+  if (canShoot) {
+    fireballs.push({
+      x: player.x + player.width / 2 - (((32*fireballRadius)/5)/2),
+      y: player.y + player.height + ((32*fireballRadius)/5),
+      radius: fireballRadius,
+      width: (32*fireballRadius)/5,
+      height: (32*fireballRadius)/5
+    });
+    canShoot = false;
+    setTimeout(() => { canShoot = true }, canShoottimer); // Cooldown for shooting
+  }
+}
+
+function drawFireball(fireball) {
+  drawAnimatedImage(fireballImage, fireball.x, fireball.y - 16, fireball.radius, 0, 2 * Math.PI, fireball.width, fireball.height);
+}
+
+function updateFireballs() {
+
+    // Se il potere è attivo, spara automaticamente palle di fuoco
+    if (powerupActive) {
+
+        createFireball();
+
+        canShoottimer = 50;
+
+        // Controlla se il potere è scaduto
+        if (Date.now() - powerupStartTime > 10000) { // 10000 millisecondi = 10 secondi
+            powerupActive = false;
+        }
+    }
+
+    for (let i = fireballs.length - 1; i >= 0; i--) {
+        fireballs[i].y -= fireballSpeed;
+
+        // Check collision with meteors
+        for (let j = meteore.length - 1; j >= 0; j--) {
+            const distanceX = fireballs[i].x - meteore[j].x;
+            const distanceY = fireballs[i].y - meteore[j].y;
+            const combinedRadius = fireballs[i].radius * 1.5 + meteore[j].radius;
+
+            if (distanceX < combinedRadius && distanceX > -combinedRadius &&
+                distanceY < combinedRadius && distanceY > -combinedRadius) {
+                meteore.splice(j, 1);
+                
+                fireballs.splice(i, 1);
+                
+                break; // Only destroy one meteor per fireball
+            }
+        }
+
+        try {
+        // Check collision with obstacles
+        for (let j = obstacles.length - 1; j >= 0; j--) {
+          const distanceX = fireballs[i].x - obstacles[j].x;
+          const distanceY = fireballs[i].y - obstacles[j].y;
+          const combinedRadius = fireballs[i].radius + obstacles[j].width / 2;
+
+          if (distanceX * distanceX + distanceY * distanceY < combinedRadius * combinedRadius) {
+            obstacles.splice(j, 1);
+
+            fireballs.splice(i, 1);
+            
+            const randomReward = Math.floor(Math.random() * 3);
+
+            switch (randomReward) {
+              case 0:
+                activatePowerup();
+                break;
+              case 1:
+                addlife();
+                break;
+              case 2:
+                immunita();
+                break;
+            }
+          }
+        }
+
+        // Remove fireballs that go off-screen
+        if (fireballs[i].y < 0) {
+          fireballs.splice(i, 1);
+        }
+        } catch {
+            console.log("già eliminata");
+        }
+    }
+}
 var hit = false;
 let livesElement = document.getElementById('lives');
 
 setInterval(() => resetHit(), 1000);
 
 function checkCollision() {
-    if (!hit) { // Controlla se 'hit' è ancora falso
-        for (let i = 0; i < obstacles.length; i++) {
-            if (
-                player.x < obstacles[i].x + obstacles[i].width &&
-                player.x + player.width > obstacles[i].x &&
-                player.y < obstacles[i].y + obstacles[i].height &&
-                player.y + player.height > obstacles[i].y
-            ) { 
-                obstacles.splice(i, 1);
+    
+    for (let i = 0; i < obstacles.length; i++) {
+        if (
+            player.x < obstacles[i].x + obstacles[i].width &&
+            player.x + player.width > obstacles[i].x &&
+            player.y < obstacles[i].y + obstacles[i].height &&
+            player.y + player.height > obstacles[i].y
+        ) { 
+            obstacles.splice(i, 1);
 
-                if(lives == 0){
-                    window.location.reload();
-                }
+            points++;
 
-                points++;
-                hit = true; // Imposta 'hit' a true
+            timerElement.textContent = "record : " + points;
 
-                timerElement.textContent = "record : " + points;
-
-                break;
-            }
+            break;
         }
+    }
         
+
+    if (!hit) { // Controlla se 'hit' è ancora falso
         for (let i = 0; i < meteore.length; i++) {
             if (
                 player.x < meteore[i].x + meteore[i].radius &&
@@ -198,10 +312,13 @@ function checkCollision() {
                 }
 
                 livesstring = "";
-                lives -= 1;
+
+                if(!ismega){
+                    lives -= 1;
+                }
                 hit = true; // Imposta 'hit' a true
 
-                for(let y = 0; y <= lives; y++){
+                for(let y = 1; y <= lives; y++){
                     livesstring = livesstring + "❤️";
                 }
                 
@@ -237,6 +354,8 @@ function update() {
     checkCollision();
     obstacles.forEach(drawObstacle);
     meteore.forEach(drawMeteor);
+    fireballs.forEach(drawFireball);
+    updateFireballs();
     requestAnimationFrame(update);
 }
 
@@ -259,42 +378,62 @@ function jump() {
     }
 }
 
- document.addEventListener('touchstart', function(event) {
+function goDown() {
+    player.dy = - player.jumpPower;
+    player.canJump = false;
+}
+
+
+// Event listeners for touch
+document.addEventListener('touchstart', handleTouchStart);
+document.addEventListener('touchend', handleTouchEnd);
+
+function handleTouchStart(event) {
     var touch = event.touches[0];
     var x = touch.clientX;
     var y = touch.clientY;
 
-    console.log(canvas.height / 5);
-    console.log(y);
-
-    if(canvas.height - y > (canvas.height / 5)){
-        jump();
+    if (canvas.height - y > (canvas.height / 5)) {
+        keyDown({ key: 'ArrowUp' });
+    } else if (canvas.height - y < (canvas.height / 5)) {
+        keyDown({ key: 'ArrowDown' });
     }
 
-    if(x < (canvas.width / 2)){
-        moveLeft();
+    if (x < (canvas.width / 2)) {
+        keyDown({ key: 'ArrowLeft' });
     } else {
-        moveRight();
+        keyDown({ key: 'ArrowRight' });
     }
+    
+    if (x > (canvas.width / 2) && y < (canvas.height / 5)) {
+        keyDown({ key: ' ' });
+    }
+}
 
-}, false);
+function handleTouchEnd(event) {
+    keyUp({ key: 'ArrowRight' });
+    keyUp({ key: 'ArrowLeft' });
+    keyUp({ key: 'ArrowUp' });
+    keyUp({ key: 'ArrowDown' });
+    keyUp({ key: ' ' });
+}
 
- document.addEventListener('touchend', function(event) {
-    stop()
-
-}, false);
 
 // Event listeners for keyboard
 document.addEventListener('keydown', keyDown);
 document.addEventListener('keyup', keyUp);
 
 function keyDown(e) {
-    if (e.key === 'ArrowRight' || e.key === 'Right') {
+    if (e.key === 'ArrowRight' || e.key === 'Right' || e.key === 'd') {
         moveRight();
-    } else if (e.key === 'ArrowLeft' || e.key === 'Left') {
+    } else if (e.key === 'ArrowLeft' || e.key === 'Left' || e.key === 'a') {
         moveLeft();
-    } else if (e.key === 'ArrowUp' || e.key === 'Up') {
+    } else if (e.key === 'ArrowUp' || e.key === 'Up' || e.key === 'w') {
         jump();
+    } else if (e.key === 'ArrowDown' || e.key === 'Down' || e.key === 's') {
+        goDown();
+    } else if (e.key === ' ' && canShoot) {
+        createFireball();
     }
 }
 
@@ -303,7 +442,9 @@ function keyUp(e) {
         e.key === 'ArrowRight' ||
         e.key === 'Right' ||
         e.key === 'ArrowLeft' ||
-        e.key === 'Left'
+        e.key === 'Left' ||
+        e.key === 'a' ||
+        e.key === 'd' 
     ) {
         stop();
     }
@@ -386,9 +527,55 @@ function updateTimer() {
     if (elapsedTime - lastUpdateTime >= 13) {
         lastUpdateTime = elapsedTime; // Update lastUpdateTime for future checks
         speedHard++;
+        player.speed++;
         // Create new meteors at regular intervals
         setInterval(createMeteor, 1500);
     }
+}
+
+// Funzione per attivare il potere
+function activatePowerup() {
+    showNotification('10 sec Power Up!', 1000);
+
+    powerupActive = true;
+    powerupStartTime = Date.now();
+}
+
+function addlife() {
+    showNotification('+1 Lives', 1000);
+    livesstring = "";
+    lives++;
+
+    for(let y = 1; y <= lives; y++){
+        livesstring = livesstring + "❤️";
+    }
+    
+    livesElement.textContent = livesstring;
+
+}
+
+let lastfireballtimeot = 0;
+let ismega = false;
+
+function immunita() {
+    showNotification('10 sec Immunity!', 1000);
+
+    ismega = true;
+
+    clearTimeout(lastfireballtimeot);
+
+    lastfireballtimeot = setTimeout(() => { fireballRadius = 5; ismega = false; }, 10000); // Cooldown for shooting
+
+}
+
+function showNotification(message, duration) {
+    const notification = document.getElementById('notification');
+    notification.innerText = message;
+    notification.style.display = 'block';
+    
+    setTimeout(function() {
+        notification.style.display = 'none';
+    }, duration);
 }
 
 setInterval(updateTimer, 100); // Aggiorna il timer ogni secondo
